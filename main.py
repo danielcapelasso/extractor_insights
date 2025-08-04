bc69b4e1d5fdb2df518fbb14556d46b6696e443a
danielcapelasso <daniel.capelasso@yalo.com>
2025-08-04 14:24:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-from fastapi import FastAPI, UploadFile, Form, Header, HTTPException
-from app import extract_insights_api  # importa do app.py
-
-app = FastAPI()
-
-# Registra o endpoint da API que já existe no app.py
-app.post("/extract-insights")(extract_insights_api)
+from app import app
